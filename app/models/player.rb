95fb014a67e00615ae5ef61263a01a51271180c8
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-08-08 21:54:20 -0400

    Player validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name
+  validates_numericality_of :jersey_number, :only_integer => true, :greater_than_or_equal_to => 1
 end
